6b6a66e4defb97a266a55921aca01ee1a429db5c
Manuel <github@mrspoocy.com>
2019-12-17 16:08:34 +0100

    add HostIds & Application Names

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,8 @@
+export declare type HostId = "PHSP" | "PHXS" | "IDSN" | "AICY" | "ILST" | "PPRO" | "PRLD" | "AEFT" | "FLPR" | "AUDT" | "DRWV" | "MUSE" | "KBRG" | "RUSH";
+declare type HostNameMap = {
+    [K in HostId]: string;
+};
+export declare const HostApplication: Partial<HostNameMap>;
 export * from './v9';
 export { CSInterface as CSInterfacePromise } from "./utils";
 export { default } from './v9';
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
+export type HostId = "PHSP" | "PHXS" | "IDSN" | "AICY" | "ILST" | "PPRO" | "PRLD" | "AEFT" | "FLPR" | "AUDT" | "DRWV" | "MUSE" | "KBRG" | "RUSH";
+
+type HostNameMap = {
+ [K in HostId]: string
+};
+
+export const HostApplication: Partial<HostNameMap> = {
+    PHSP: "Photohsop",
+    PHXS: "Photoshop",
+    IDSN: "InDesign",
+    AICY: "InCopy",
+    ILST: "Illustrator",
+    PPRO: "Premiere Pro",
+    PRLD: "Prelude",
+    AEFT: "After Effects",
+    FLPR: "Animate (Flash Pro)",
+    AUDT: "Audition",
+    DRWV: "Dreamweaver",
+    MUSE: "Muse",
+    KBRG: "Bridge",
+    RUSH: "Rush"
+};
+
 export * from './v9';
 export {CSInterface as CSInterfacePromise} from "./utils";
 export {default} from './v9'
